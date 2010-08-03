20ea256e29d9f695636248c594a713367506a262
rfonseca <rfonseca@53b6c913-c175-4e75-a8ba-878503ff61f7>
2010-08-03 19:00:24 +0000

    Rasmus .. initial ProGAL.math package
    git-svn-id: svn+ssh://ask.diku.dk/usr/local/projects/disk03/proteins/repo/ProGAL/Java@502 53b6c913-c175-4e75-a8ba-878503ff61f7

--- src/ProGAL/math/Combinatorics.java
+++ src/ProGAL/math/Combinatorics.java
@@ -1,61 +1,21 @@
 package ProGAL.math;
 
-import java.util.ArrayList; 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Random {
-	private static java.util.Random rand = new java.util.Random(); 
-
-	/**
-	 * Return a uniform random number between (including) i1 and (not including) i2. 
-	 */
-	public static int randBetween(int i1, int i2){
-		int max = Math.max(i1, i2);
-		int min = Math.min(i1, i2);
-		return (int)(rand.nextDouble()*(max-min)+min);
-	}
-	/**
-	 * Return a uniform random double between (including) d1 and (not including) d2
-	 */
-	public static double randBetween(double d1, double d2){
-		double max = Math.max(d1, d2);
-		double min = Math.min(d1, d2);
-		return rand.nextDouble()*(max-min)+min;
-	}
-
-
-	/**
-	 * Generate a random permutation of integers between (including) 0 and (not including) max
-	 */
-	public static int[] randomPermutation(int max){
-		int[] ret = new int[max];
-		for(int i=0;i<max;i++){ ret[i] = i;}
-		randomizeInPlace(ret);
-		return ret;
-	}
-	
-	/**
-	 * Randomize the array as.
-	 * @param as the array to be randomized
-	 */
-	public static void randomizeInPlace(int[] as){
-		for(int i=0;i<as.length;i++){
-			int r = randBetween(i,as.length);
-			int t = as[i];
-			as[i] = as[r];
-			as[r] = t;
-		}
-	}
+/** 
+ * A wrapper for static combinatorial helper functions.
+ * @author rfonseca
+ */
+public class Combinatorics {
 
 	/**
 	 * Generates all permutations of integers from 0 (inclusive) to max (exclusive).
-	 * TODO: Move this method
 	 * @param max the length of each permutation
 	 * @return a list of all permutations of length max
 	 */
 	public static List<int[]> getAllPermutations(int max){
 		int[] begin = {};
-
 		int[] end = new int[max];
 		for(int i=0;i<max;i++) end[i] = i;
 		
@@ -94,7 +54,6 @@ public class Random {
 
 	/**
 	 * Calculate the binomial coefficient.
-	 * TODO: Move this method
 	 * @param n a number larger than or equal 0
 	 * @param k a number larger than or equal 0 but smaller than or equal n
 	 * @return n choose k
@@ -111,11 +70,4 @@ public class Random {
 		return b[m];
 	}
 
-	/**
-	 * Seeds the random generater used by the randPermutation and randBetween methods.
-	 * @param s
-	 */
-	public static void seed(long s){
-		rand = new java.util.Random(s);
-	}
 }
