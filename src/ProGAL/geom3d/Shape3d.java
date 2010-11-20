9cde65cf98632a99392b45998246d3f2e1e80bfe
rfonseca <rfonseca@53b6c913-c175-4e75-a8ba-878503ff61f7>
2010-11-20 01:33:21 +0000

    git-svn-id: svn+ssh://ask.diku.dk/usr/local/projects/disk03/proteins/repo/ProGAL/Java@595 53b6c913-c175-4e75-a8ba-878503ff61f7

--- src/ProGAL/geom3d/Shape3d.java
+++
@@ -1,9 +0,0 @@
-package ProGAL.geom3d;
-
-/**
- * An interface for 3d shapes such as spheres, but also 
- * flat objects such as rectangles and text. 
- */
-public interface Shape3d {
-	public Point3d getCenter();
-}
