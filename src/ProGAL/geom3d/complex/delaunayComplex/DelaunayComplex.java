30f92f55da48b6798b1fed0ba89777f722ca2a87
rfonseca <rfonseca@53b6c913-c175-4e75-a8ba-878503ff61f7>
2010-12-25 16:13:09 +0000

    git-svn-id: svn+ssh://ask.diku.dk/usr/local/projects/disk03/proteins/repo/ProGAL/Java@626 53b6c913-c175-4e75-a8ba-878503ff61f7

--- src/ProGAL/geom3d/complex/delaunayComplex/DelaunayComplex.java
+++ src/ProGAL/geom3d/complex/delaunayComplex/DelaunayComplex.java
@@ -16,8 +16,7 @@ import ProGAL.geom3d.complex.SimplicialComplex;
 import ProGAL.geom3d.predicates.*;
 import ProGAL.geom3d.predicates.Predicates.SphereConfig;
 
-/** 
- *  <p>
+/** <p>
  *  A Delaunay complex for a set of d-dimensional points is a tesselation of the points such that no point is inside 
  *  the circumscribing hypersphere of the d-simplices (for the 3D case: Tetrahedra). 
  *  </p>
@@ -27,18 +26,20 @@ import ProGAL.geom3d.predicates.Predicates.SphereConfig;
  *  <code>getTetrahedra</code> method. The following example displays the Delaunay complex of ten random points. 
  *  <pre>
  *  {@code
- *    //Generate the complex
- *    List<Point> pl = PointList.generatePointsInCube(10);
- *    DelaunayComplex dc = new DelaunayComplex(pl);
+ *  //Generate the complex
+ *  List<Point> pl = PointList.generatePointsInCube(10);
+ *  DelaunayComplex dc = new DelaunayComplex(pl);
  *    
- *    //Display the complex
- *    J3DScene scene = J3DScene.createJ3DSceneInFrame();
- *    for(CTetrahedron t: dc.getTetrahedra()){
- *      scene.addShape(t, new Color(200,100,100,100));
- *    }
+ *  //Display the complex
+ *  J3DScene scene = J3DScene.createJ3DSceneInFrame();
+ *  for(CTetrahedron t: dc.getTetrahedra()){
+ *    scene.addShape(t, new Color(200,100,100,100));
+ *  }
  *  }
  *  </pre>     
  *  </p>
+ *  
+ *  @author R.Fonseca
  */
 public class DelaunayComplex implements SimplicialComplex{
 
@@ -167,7 +168,7 @@ public class DelaunayComplex implements SimplicialComplex{
 		}
 	}
 
-	/** Checks that the tetrahedra comply with the Delaunay-criteria. */
+	/** Checks that all tetrahedra comply with the Delaunay-criteria. */
 	public boolean checkTetrahedra() {
 		for(CTetrahedron t: flips.getTetrahedrastack()){
 			for(CVertex p: points){
