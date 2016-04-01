5992f00243d907a5b6264c75b943b7ecc6d00aff
Rasmus Fonseca <rfonseca@Rasmuss-iMac.local>
2016-04-01 11:11:50 -0700

    Cleaning up a bit

--- src/ProGAL/geom3d/tess/TCorner.java
+++
@@ -1,8 +0,0 @@
-package ProGAL.geom3d.tess;
-
-public class TCorner {
-	TPoint v; // index of vertex
-	int opp; // pointer to opposite corner in neighboring tet
-	TSphere plane; // plane equation of separating plane (v * plane > 0)
-
-}
--- src/ProGAL/geom3d/tess/TSphere.java
+++
@@ -1,5 +0,0 @@
-package ProGAL.geom3d.tess;
-
-public class TSphere {
-	double w, x, y, z, sq;
-}
